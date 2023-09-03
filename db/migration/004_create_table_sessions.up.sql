3c902d118f4e497e1e64099d04210ad9bcaabb27
Linggar Juwita <linggarjuwita@gmail.com>
2023-09-04 00:52:54 +0800

    Added column refreshed_at to table sessions

--- db/migration/004_create_table_sessions.up.sql
+++ db/migration/004_create_table_sessions.up.sql
@@ -2,9 +2,10 @@ CREATE TABLE IF NOT EXISTS sessions (
     id UUID PRIMARY KEY DEFAULT uuid_generate_v4() NOT NULL,
     user_id UUID NOT NULL,
     token UUID NOT NULL,
+    refreshed_at TIMESTAMP NOT NULL,
     expires_at TIMESTAMP NOT NULL,
     created_at TIMESTAMP DEFAULT NOW(),
-    last_updated_at TIMESTAMP NOT NULL,
+    last_updated_at TIMESTAMP,
     CONSTRAINT uidx_sessions_user_id UNIQUE (user_id)
 );
 
--- db/test/table_sessions.sql
+++ db/test/table_sessions.sql
@@ -1,5 +1,5 @@
 BEGIN;
-SELECT plan(21);
+SELECT plan(23);
 
 SELECT has_table('sessions');
 
@@ -15,6 +15,10 @@ SELECT has_column('sessions', 'token');
 SELECT col_type_is('sessions', 'token', 'uuid');
 SELECT col_not_null('sessions', 'token');
 
+SELECT has_column('sessions', 'refreshed_at');
+SELECT col_type_is('sessions', 'refreshed_at', 'timestamp without time zone');
+SELECT col_not_null('sessions', 'refreshed_at');
+
 SELECT has_column('sessions', 'expires_at');
 SELECT col_type_is('sessions', 'expires_at', 'timestamp without time zone');
 SELECT col_not_null('sessions', 'expires_at');
@@ -25,7 +29,6 @@ SELECT col_default_is('sessions', 'created_at', 'now()');
 
 SELECT has_column('sessions', 'last_updated_at');
 SELECT col_type_is('sessions', 'last_updated_at', 'timestamp without time zone');
-SELECT col_not_null('sessions', 'last_updated_at');
 
 SELECT index_is_unique('sessions', 'uidx_sessions_user_id');
 SELECT has_index('sessions', 'idx_sessions_token');
