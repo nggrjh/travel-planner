f9c905c23a4d0077129332afab99f170afbd71c8
Linggar Juwita <linggarjuwita@gmail.com>
2023-09-04 00:40:06 +0800

    Added column last_updated_at to table sessions

--- db/migration/004_create_table_sessions.up.sql
+++ db/migration/004_create_table_sessions.up.sql
@@ -2,8 +2,9 @@ CREATE TABLE IF NOT EXISTS sessions (
     id UUID PRIMARY KEY DEFAULT uuid_generate_v4() NOT NULL,
     user_id UUID NOT NULL,
     token UUID NOT NULL,
-    created_at TIMESTAMP DEFAULT NOW(),
     expires_at TIMESTAMP NOT NULL,
+    created_at TIMESTAMP DEFAULT NOW(),
+    last_updated_at TIMESTAMP NOT NULL,
     CONSTRAINT uidx_sessions_user_id UNIQUE (user_id)
 );
 
